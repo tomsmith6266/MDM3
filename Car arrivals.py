33f6ac67ef98c5550d636e69865045d40fd0414d
Joseph Lewis <60779181+pepe100000@users.noreply.github.com>
2021-10-22 09:30:56 +0100

    Add files via upload
    Cars arriving randomly using poisson distribution

---
+++ Car arrivals.py
@@ -0,0 +1,46 @@
+from scipy.stats import poisson
+
+
+
+# set variables
+Capacity = 150;      #CarPark capacity
+TPeriod = 10;        #Arrival Time period (minutes)
+Interval = 1;       #Interval of time you want to measure the number of cars arriving (seconds)
+
+
+#Calculate the mean for the poisson distribution
+TPeriod = TPeriod*60;
+mean = Capacity/(TPeriod/Interval);
+print(mean);        #Number of cars arriving per second over the 60 minute period.
+
+
+#Start the poisson distribution
+data_poisson = poisson.rvs(mu=mean, size=TPeriod);
+
+
+#checking outcome is as expected
+print(data_poisson);
+print(sum(data_poisson));
+print(len(data_poisson));
+
+#sink variable
+sink = 0;
+
+#for loop to separate arriving cars by units of time (never more than one car arriving each second)
+for i in range(len(data_poisson)):
+    if data_poisson[i] > 1:
+        while data_poisson[i] > 1:
+            data_poisson[i] -= 1;
+            sink += 1;
+    elif data_poisson[i] == 0 and sink > 0:
+        data_poisson[i] += 1;
+        sink -= 1;
+    else:
+        continue
+
+
+#checking outcome is as expected
+print(data_poisson);
+print(sum(data_poisson));
+print(len(data_poisson));
+
